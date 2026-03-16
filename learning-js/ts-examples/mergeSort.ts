4493e262bbc1bf53a522f7a12264cef8a1ceb3be
Pullabhatla Ram Swaroop <140191636+SwaroopSRP@users.noreply.github.com>
2026-03-16 13:19:20 +0530

    Add merge sort implementation in TypeScript
    Implement merge sort algorithm with optional reverse sorting.

---
+++ learning-js/ts-examples/mergeSort.ts
@@ -0,0 +1,53 @@
+type Sortable = number | string;
+
+function merge<T extends Sortable>(
+    arr: T[],
+    left: number,
+    mid: number,
+    right: number
+) {
+    const leftArr = arr.slice(left, mid + 1);
+    const rightArr = arr.slice(mid + 1, right + 1);
+
+    let i = 0;
+    let j = 0;
+    let k = left;
+
+    while (i < leftArr.length && j < rightArr.length) {
+        if (leftArr[i]! <= rightArr[j]!) {
+            arr[k++] = leftArr[i++]!;
+        } else {
+            arr[k++] = rightArr[j++]!;
+        }
+    }
+
+    while (i < leftArr.length) {
+        arr[k++] = leftArr[i++]!;
+    }
+
+    while (j < rightArr.length) {
+        arr[k++] = rightArr[j++]!;
+    }
+}
+
+function mergeSort<T extends Sortable>(arr: T[], left = 0, right = arr.length - 1, reverse = false) {
+    if (left >= right) {
+        return;
+    }
+
+    const mid = Math.floor((left + right) / 2);
+
+    mergeSort(arr, left, mid);
+    mergeSort(arr, mid + 1, right);
+
+    merge(arr, left, mid, right);
+
+    if (reverse) {
+        arr.reverse();
+    }
+}
+
+
+const arr1 = [5, 2, 9, 1, 5, 6];
+mergeSort(arr1, undefined, undefined, true);
+console.log(arr1);
